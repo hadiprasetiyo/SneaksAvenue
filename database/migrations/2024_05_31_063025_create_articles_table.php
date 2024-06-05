46891ad2fbfd8be7ab691a0ae2daa4916d064439
hadiprasetiyo <hadiiyok01@gmail.com>
2024-06-05 16:07:39 +0800

    feat: add migration file for creating 'articles' table

---
+++ database/migrations/2024_05_31_063025_create_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+      Schema::create('articles', function (Blueprint $table) {
+        $table->id();
+        $table->string('image');
+        $table->string('title');
+        $table->string('price');
+        $table->unsignedBigInteger('varian_id')->nullable(false)->default(null);
+        $table->foreign('varian_id')->references('id')->on('varian')->onUpdate('cascade')->onDelete('cascade');
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};

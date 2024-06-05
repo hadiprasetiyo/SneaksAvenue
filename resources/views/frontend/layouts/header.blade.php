1b703d4a7d0c16453e235c6139f04a57f9262d5e
hadiprasetiyo <hadiiyok01@gmail.com>
2024-06-05 15:11:18 +0800

    feat(frontend): add header layout with logo and navigation

---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,16 @@
+<header id="header" class="fixed-top  header-transparent ">
+  <div class="container d-flex align-items-center justify-content-between">
+
+    <div class="logo">
+      <h1>SneaksAvenue</h1>
+    </div>
+
+    <nav id="navbar" class="navbar">
+      <ul>
+        <li><a class="getstarted scrollto" href="{{route('article.index')}}">Yuk Login</a></li>
+      </ul>
+      <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav>
+
+  </div>
+</header>
